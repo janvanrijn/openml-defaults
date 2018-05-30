bdf24dcb20c0253e66d4ba9daabea3c6455795ec
janvanrijn <janvanrijn@gmail.com>
2018-05-29 20:50:30 -0400

    updated greede output

--- openmldefaults/models/greedy.py
+++ openmldefaults/models/greedy.py
@@ -13,15 +13,21 @@ class GreedyDefaults(object):
         start_time = time.time()
 
         selected_configs = []
+        selected_indices = []
         for itt_defaults in range(num_defaults):
             best_addition = None
             best_score = None
-            for current_config in df.index.values:
+            best_index = None
+            for idx, current_config in enumerate(df.index.values):
                 current_score = sum(openmldefaults.utils.selected_set(df, selected_configs + [current_config]))
                 if best_score is None or current_score < best_score:
                     best_score = current_score
                     best_addition = current_config
+                    best_index = idx
             selected_configs.append(best_addition)
+            selected_indices.append(best_index)
+
+        print(openmldefaults.utils.get_time(), {'solution': sorted(selected_indices), 'score': best_score})
 
         runtime = time.time() - start_time
         results_dict = {
