25230eddd119d1a3f2424e1e227cf9b424c63020
janvanrijn <janvanrijn@gmail.com>
2018-06-14 17:46:46 -0400

    paralelized cv iteration

--- examples/generate_default_holdout_multi.py
+++ examples/generate_default_holdout_multi.py
@@ -11,6 +11,7 @@ def parse_args():
     parser.add_argument('--max_num_defaults', type=int, default=10)
     parser.add_argument('--c_executable', type=str, default='../c/main')
     parser.add_argument('--cv_iterations', type=int, default=10)
+    parser.add_argument('--cv_iteration', type=int, default=None)
     parser.add_argument('--output_dir', type=str, default=os.path.expanduser('~') + '/experiments/openml-defaults')
     return parser.parse_args()
 
@@ -32,7 +33,12 @@ if __name__ == '__main__':
 
     for num_defaults in range(1, args.max_num_defaults + 1):
         for (dataset_train_path, num_tasks, flip_performances, params) in datasets:
-            for cv_iteration in range(args.cv_iterations):
+
+            cv_iterations = list(range(args.cv_iterations))
+            if args.cv_iteration is not None:
+                cv_iterations = [args.cv_iteration]
+
+            for cv_iteration in cv_iterations:
                 holdout_tasks = openmldefaults.utils.get_cv_indices(num_tasks, args.cv_iterations, cv_iteration)
                 generate_defaults(dataset_train_path, flip_performances, params, args.resized_grid_size, num_defaults,
                                   args.c_executable, args.output_dir)
