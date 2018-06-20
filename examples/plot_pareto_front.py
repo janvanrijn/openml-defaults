0943cd905bc6ab4e8a87fcbf72744a7bc62a8c73
janvanrijn <janvanrijn@gmail.com>
2018-06-20 14:07:34 -0400

    updated plot pareto

--- examples/plot_pareto_front.py
+++ examples/plot_pareto_front.py
@@ -1,17 +1,23 @@
 import argparse
+import collections
 import matplotlib.pyplot as plt
 import numpy as np
 import openmldefaults
 import os
+import pickle
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset_path', type=str,
                         default=os.path.expanduser('~') + '/data/openml-defaults/svm-ongrid.arff')
+    parser.add_argument('--experiment_dir', type=str,
+                        default=os.path.expanduser('~') + '/experiments/openml-defaults/')
     parser.add_argument('--flip_performances', action='store_true')
     parser.add_argument('--params', type=str, nargs='+', required=True)
+    parser.add_argument('--num_params_plot', type=int, default=3)
     parser.add_argument('--resized_grid_size', type=int, default=16)
+    parser.add_argument('--num_defaults', type=int, default=3)
     parser.add_argument('--output_dir', type=str, default=os.path.expanduser('~') + '/experiments/openml-defaults')
     return parser.parse_args()
 
@@ -21,42 +27,56 @@ def rand_jitter(arr):
     return arr + np.random.randn(len(arr)) * stdev
 
 
-def plot_numeric(ax, x_series_dominated, y_series_dominated, x_series_pareto, y_series_pareto):
-    ax.scatter(rand_jitter(x_series_dominated.tolist()), rand_jitter(y_series_dominated.tolist()), color='green')
-    ax.scatter(rand_jitter(x_series_pareto.tolist()), rand_jitter(y_series_pareto.tolist()), color='red')
+def plot_numeric(ax, name, x_series_dominated, y_series_dominated):
+    ax.scatter(rand_jitter(x_series_dominated.tolist()), rand_jitter(y_series_dominated.tolist()), label=name)
 
 
 def run(args):
-    df = openmldefaults.utils.load_dataset(args.dataset_path,
-                                           args.params,
-                                           args.resized_grid_size,
-                                           args.flip_performances)
+    df_orig = openmldefaults.utils.load_dataset(args.dataset_path,
+                                                args.params,
+                                                args.resized_grid_size,
+                                                args.flip_performances)
 
-    df, dominated = openmldefaults.utils.simple_cull(df, openmldefaults.utils.dominates_min)
+    category_df = collections.OrderedDict()
+    category_df['pareto'], category_df['dominated'] = openmldefaults.utils.simple_cull(df_orig, openmldefaults.utils.dominates_min)
 
-    df = df.reset_index()
-    dominated = dominated.reset_index()
+    plot_params = args.params[:args.num_params_plot]
+    params_per_axis = len(plot_params)-1
+    fig, axes = plt.subplots(params_per_axis, params_per_axis, figsize=(params_per_axis*8, params_per_axis*6))
 
-    plot_params = ['c', 'gamma', 'degree']
-    fig, axes = plt.subplots(len(plot_params)-1, len(plot_params)-1)
+    dataset_name = os.path.basename(args.dataset_path)
+    experiment_dataset_dir = os.path.join(args.experiment_dir, dataset_name)
+    for solver_name in os.listdir(experiment_dataset_dir):
+        results_file = os.path.join(experiment_dataset_dir,
+                                    solver_name,
+                                    openmldefaults.utils.get_setup_dirname(args.resized_grid_size, args.num_defaults),
+                                    'results.pkl')
+        if not os.path.isfile(results_file):
+            print(openmldefaults.utils.get_time(), 'Warning: No results.pkl for %s' % solver_name)
+            continue
+        with open(results_file, 'rb') as fp:
+            results = pickle.load(fp)
+            category_name = '%s_d%d' % (solver_name, args.num_defaults)
+            category_df[category_name] = df_orig.loc[results['defaults']]
+            if len(category_df[category_name]) != args.num_defaults:
+                raise ValueError()
 
     for i in range(len(plot_params)-1):
         for j in range(i + 1, len(plot_params)):
-            x_series_dominated = dominated[plot_params[i]]
-            y_series_dominated = dominated[plot_params[j]]
-            x_series_pareto = df[plot_params[i]]
-            y_series_pareto = df[plot_params[j]]
-
-            if x_series_dominated.dtype != x_series_pareto.dtype:
-                raise ValueError()
-            if y_series_dominated.dtype != y_series_pareto.dtype:
-                raise ValueError()
-
             current_ax = axes[i, j-1]
-            plot_numeric(current_ax, x_series_dominated, y_series_dominated, x_series_pareto, y_series_pareto)
             current_ax.set_xlabel(plot_params[i])
             current_ax.set_ylabel(plot_params[j])
-    plt.show()
+
+            for name, df in category_df.items():
+                current = df.reset_index()
+                x_series_dominated = current[plot_params[i]]
+                y_series_dominated = current[plot_params[j]]
+                plot_numeric(current_ax, name, x_series_dominated, y_series_dominated)
+
+    axes[0, 0].legend()
+    plt.tight_layout()
+    os.makedirs(args.output_dir, exist_ok=True)
+    plt.savefig(os.path.join(args.output_dir, 'params_%s.png' % dataset_name))
 
 
 if __name__ == '__main__':
