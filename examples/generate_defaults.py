3e1a8fc92d48a8f30aec1ad95170e2422bedcff7
janvanrijn <janvanrijn@gmail.com>
2018-07-25 12:31:41 -0400

    factorized generate defaults

--- examples/generate_defaults.py
+++ examples/generate_defaults.py
@@ -18,6 +18,34 @@ def parse_args():
     return parser.parse_args()
 
 
+def _run_single_solver(model, df, dataset_path, resized_grid_size, num_defaults, output_dir, holdout_indices):
+    print(openmldefaults.utils.get_time(), 'Started on model: %s' % model.name)
+    solver_dir = model.name
+    dataset_dir = os.path.basename(dataset_path)
+    setup_dir = openmldefaults.utils.get_setup_dirname(resized_grid_size, num_defaults)
+    experiment_dir = os.path.join(output_dir, dataset_dir, 'generated_defaults', solver_dir, setup_dir)
+    if len(holdout_indices) > 0:
+        experiment_dir = os.path.join(experiment_dir, str(sorted(holdout_indices)))
+    print(openmldefaults.utils.get_time(), 'Output directory: %s' % experiment_dir)
+
+    experiment_file = os.path.join(experiment_dir, 'generated_defaults.pkl')
+    if os.path.isfile(experiment_file):
+        with open(experiment_file, 'rb') as fp:
+            print(openmldefaults.utils.get_time(), 'Already exists')
+            return pickle.load(fp)
+
+    results_dict = model.generate_defaults(df, num_defaults)
+    os.makedirs(experiment_dir, exist_ok=True)
+    with open(experiment_file, 'wb') as fp:
+        pickle.dump(results_dict, fp)
+
+    sum_of_scores = sum(openmldefaults.utils.selected_set(df, results_dict['indices']))
+    diff = abs(sum_of_scores - results_dict['objective'])
+    assert diff < 0.0001, 'Sum of scores does not equal score of solution: %f vs %f' % (sum_of_scores,
+                                                                                        results_dict['score'])
+    return results_dict
+
+
 def run(dataset_path, flip_performances, params, resized_grid_size, num_defaults, models, output_dir,
         holdout_indices=[], determine_pareto=True):
     if not isinstance(holdout_indices, list):
@@ -43,34 +71,11 @@ def run(dataset_path, flip_performances, params, resized_grid_size, num_defaults
     results = {}
 
     for model in models:
-        print(openmldefaults.utils.get_time(), 'Started on model: %s' % model.name)
-        solver_dir = model.name
-        dataset_dir = os.path.basename(dataset_path)
-        setup_dir = openmldefaults.utils.get_setup_dirname(resized_grid_size, num_defaults)
-        experiment_dir = os.path.join(output_dir, dataset_dir, 'generated_defaults', solver_dir, setup_dir)
-        if len(holdout_indices) > 0:
-            experiment_dir = os.path.join(experiment_dir, str(sorted(holdout_indices)))
-        print(openmldefaults.utils.get_time(), 'Output directory: %s' % experiment_dir)
-
-        experiment_file = os.path.join(experiment_dir, 'generated_defaults.pkl')
-        if os.path.isfile(experiment_file):
-            with open(experiment_file, 'rb') as fp:
-                print(openmldefaults.utils.get_time(), 'Already exists')
-                results[model.name] = pickle.load(fp)
-                continue
-
-        results_dict = model.generate_defaults(df, num_defaults)
-        os.makedirs(experiment_dir, exist_ok=True)
-        with open(experiment_file, 'wb') as fp:
-            pickle.dump(results_dict, fp)
-
-        sum_of_scores = sum(openmldefaults.utils.selected_set(df, results_dict['indices']))
-        diff = abs(sum_of_scores - results_dict['objective'])
-        assert diff < 0.0001, 'Sum of scores does not equal score of solution: %f vs %f' % (sum_of_scores,
-                                                                                            results_dict['score'])
-
-        results[model.name] = results_dict
-        print(results_dict)
+        results[model.name] = _run_single_solver(model=model, df=df, dataset_path=dataset_path,
+                                                 resized_grid_size=resized_grid_size,
+                                                 num_defaults=num_defaults, output_dir=output_dir,
+                                                 holdout_indices=holdout_indices)
+        print(results[model.name])
 
     if 'cpp_bruteforce' in results and 'mip' in results:
         diff = abs(results['cpp_bruteforce']['objective'] - results['mip']['objective'])
