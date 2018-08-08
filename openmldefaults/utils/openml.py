ab25530e5bfc994e174f9b1952ca16c1cb46d055
janvanrijn <janvanrijn@gmail.com>
2018-08-08 19:01:27 -0400

    added openml sklearn measure

--- openmldefaults/utils/openml.py
+++ openmldefaults/utils/openml.py
@@ -5,7 +5,7 @@ def openml_sklearn_metric_mapping(openml_metric):
     # maps from OpenML name to sklearn name
     mapping = {
         'predictive_accuracy': 'accuracy',
-        'f_measure': 'TODO!!!', # TODO: figure out which f1 calculation OpenML uses
+        'f_measure': 'f1_weighted'
     }
     if openml_metric not in mapping:
         raise ValueError('Could not find sklearn metric for openml measure: %s'
