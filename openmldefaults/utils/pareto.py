29963cd08c883120d1829fb1a18f79e1670ebf5b
janvanrijn <janvanrijn@gmail.com>
2018-09-04 11:53:44 -0400

    pareto to work with nans in key

--- openmldefaults/utils/pareto.py
+++ openmldefaults/utils/pareto.py
@@ -2,12 +2,30 @@ import pandas as pd
 from typing import Callable
 
 
+def dominates(dominater, dominated, original_index_keys=None):
+    if dominater.index.names != dominated.index.names:
+        raise ValueError()
+    if original_index_keys is None:
+        original_index_keys = []
+    total = 0
+    for key in dominater.keys():
+        if dominater[key] >= dominated[key] and key not in original_index_keys:
+            total += 1
+    return total == len(dominater) - len(original_index_keys)
+
+
 def dominates_min(dominater, dominated):
     return sum([dominater[x] <= dominated[x] for x in range(len(dominater))]) == len(dominater)
 
 
 # fn from: http://code.activestate.com/recipes/578287-multidimensional-pareto-front/
 def simple_cull(original_frame: pd.DataFrame, dominates: Callable):
+    original_frame = pd.DataFrame.copy(original_frame)
+    original_keys = original_frame.index.names
+    if original_frame.index.names == [None]:
+        raise ValueError('Frame does not have valid index')
+    original_frame.reset_index(inplace=True)
+
     len_original_frame = len(original_frame)
     # copies the frame. contains the same number of rows (filled with na's) should be removed later
     pareto_frame = pd.DataFrame(data=None, columns=original_frame.columns, index=original_frame.index)
@@ -16,38 +34,36 @@ def simple_cull(original_frame: pd.DataFrame, dominates: Callable):
 
     while True:
         # obtain the current row
-        candidate_key = original_frame.index[candidate_row_nr]
-        candidate_row = original_frame.loc[candidate_key]
+        candidate_row = original_frame.iloc[candidate_row_nr]
         # remove it from the original frame
-        original_frame = original_frame.drop(candidate_key, axis=0)
+        original_frame = original_frame.drop(original_frame.index[candidate_row_nr], axis=0)
 
         # iterate over other rows
         alternative_row_nr = 0
         non_dominated = True
         while len(original_frame) != 0 and alternative_row_nr < len(original_frame):
-            alternative_key = original_frame.index[alternative_row_nr]
-            alternative_row = original_frame.loc[alternative_key]
-            if dominates(candidate_row, alternative_row):
+            alternative_row = original_frame.iloc[alternative_row_nr]
+            if dominates(candidate_row, alternative_row, original_keys):
                 # Candidate row is dominated so remove it from the array
-                original_frame = original_frame.drop(alternative_key, axis=0)
-                dominated_frame.loc[alternative_key] = alternative_row
-            elif dominates(alternative_row, candidate_row):
+                original_frame = original_frame.drop(alternative_row_nr, axis=0)
+                dominated_frame.iloc[alternative_row_nr] = alternative_row
+
+            elif dominates(alternative_row, candidate_row, original_keys):
                 non_dominated = False
-                dominated_frame.loc[candidate_key] = candidate_row
+                dominated_frame.iloc[candidate_row_nr] = candidate_row
                 alternative_row_nr += 1
             else:
                 alternative_row_nr += 1
 
         if non_dominated:
             # add the non-dominated point to the Pareto frontier
-            pareto_frame.loc[candidate_key] = candidate_row
+            pareto_frame.loc[alternative_row_nr] = candidate_row
 
         if len(original_frame) == 0:
             break
 
-    pareto_frame = pareto_frame.dropna()
-    dominated_frame = dominated_frame.dropna()
-
+    pareto_frame = pareto_frame.set_index(original_keys).dropna()
+    dominated_frame = dominated_frame.set_index(original_keys).dropna()
     if len(pareto_frame) + len(dominated_frame) != len_original_frame:
         raise ValueError('Original frame has %d rows. Pareto %d, Dominated %d' % (len_original_frame,
                                                                                   len(pareto_frame),
--- tests/test_pareto.py
+++ tests/test_pareto.py
@@ -6,25 +6,25 @@ import unittest
 class TestParetoFunctions(unittest.TestCase):
 
     def test_simple_cull(self):
-        def dominates(dominater, dominated):
-            return sum([dominater[x] >= dominated[x] for x in range(len(dominater))]) == len(dominater)
+        frame = pd.DataFrame(data=[[0.0, 1.0, 1.0, 1.0],
+                                   [1.0, 1.0, 2.0, 3.0],
+                                   [2.0, 3.0, 2.0, 1.0],
+                                   [3.0, 4.0, 1.0, 1.0]],
+                             columns=['idx', 'task 1', 'task 2', 'task 3'],
+                             dtype=float).set_index('idx')
 
-        frame = pd.DataFrame(data=[[1, 1, 1], [1, 2, 3], [3, 2, 1], [4, 1, 1]],
-                             columns=['task 1', 'task 2', 'task 3'],
-                             dtype=int)
-
-        pareto_points, dominated_points = openmldefaults.utils.simple_cull(frame, dominates)
-        reconstructed = pareto_points.append(dominated_points).sort_index().astype(int)
+        pareto_points, dominated_points = openmldefaults.utils.simple_cull(frame, openmldefaults.utils.dominates)
+        reconstructed = pareto_points.append(dominated_points).sort_index().astype(float)
         pd.testing.assert_frame_equal(frame, reconstructed)
 
     def test_simple_cull_multi_level_index(self):
-        def dominates(dominater, dominated):
-            return sum([dominater[x] >= dominated[x] for x in range(len(dominater))]) == len(dominater)
-
-        frame = pd.DataFrame(data=[[0, 0, 1, 1, 1], [0, 1, 1, 2, 3], [1, 0, 3, 2, 1], [1, 1, 4, 1, 1]],
+        frame = pd.DataFrame(data=[[0.0, 0.0, 1.0, 1.0, 1.0],
+                                   [0.0, 1.0, 1.0, 2.0, 3.0],
+                                   [1.0, 0.0, 3.0, 2.0, 1.0],
+                                   [1.0, 1.0, 4.0, 1.0, 1.0]],
                              columns=['idx0', 'idx1', 'task 1', 'task 2', 'task 3'],
-                             dtype=int).set_index(['idx0', 'idx1'])
+                             dtype=float).set_index(['idx0', 'idx1'])
 
-        pareto_points, dominated_points = openmldefaults.utils.simple_cull(frame, dominates)
-        reconstructed = pareto_points.append(dominated_points).sort_index().astype(int)
+        pareto_points, dominated_points = openmldefaults.utils.simple_cull(frame, openmldefaults.utils.dominates)
+        reconstructed = pareto_points.append(dominated_points).sort_index().astype(float)
         pd.testing.assert_frame_equal(frame, reconstructed)
