e0e05b859e3608d504aad169fee32a0637dc30ab
janvanrijn <janvanrijn@gmail.com>
2018-10-17 22:09:49 -0400

    improved plotting script

--- examples/symbolic/plot.py
+++ examples/symbolic/plot.py
@@ -7,10 +7,27 @@ import seaborn as sns
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--results_dir', type=str, default=os.path.expanduser('~') + '/experiments/openml-defaults/symbolic_defaults/')
+    parser.add_argument('--results_dir', type=str, default=os.path.expanduser('~') + '/habanero_experiments/openml-defaults/symbolic_defaults')
+    parser.add_argument('--classifier_name', type=str, default='libsvm_svc')
+
     parser.add_argument('--output_dir', type=str, default=os.path.expanduser('~') + '/Desktop/')
     parser.add_argument('--per_task', action='store_true', default=True)
     return parser.parse_args()
+
+
+def formula_str(result):
+    if result['transform_fn'] == 'inverse_transform_fn':
+        return '%f / %s ' % (result['transform_alpha_value'], result['transform_meta_feature'])
+    elif result['transform_fn'] == 'power_transform_fn':
+        return '%f^{%s} ' % (result['transform_alpha_value'], result['transform_meta_feature'])
+    elif result['transform_fn'] == 'multiply_transform_fn':
+        return '%f \\cdot %s ' % (result['transform_alpha_value'], result['transform_meta_feature'])
+    elif result['transform_fn'] == 'log_transform_fn':
+        return '%f \\cdot \\log %s ' % (result['transform_alpha_value'], result['transform_meta_feature'])
+    elif result['transform_fn'] == 'root_transform_fn':
+        return '%f \\cdot \\sqrt{%s} ' % (result['transform_alpha_value'], result['transform_meta_feature'])
+    else:
+        raise ValueError()
 
 
 def get_results_train(directory):
@@ -20,15 +37,17 @@ def get_results_train(directory):
     baseline_result = results['baseline_results_per_task']
     best_avg_score = results['baseline_avg_performance']
     best_result = None
+    best_meta_formula = None
 
     for result in results['symbolic_defaults']:
         if result['avg_performance'] > best_avg_score:
             best_avg_score = result['avg_performance']
             best_result = result['results_per_task']
+            best_meta_formula = formula_str(result)
 
     data = []
     for idx in range(len(best_result)):
-        data.append({'defaults_type': 'symbolic', 'task_idx': idx, 'evaluation': best_result[idx]})
+        data.append({'defaults_type': 'symbolic', 'task_idx': idx, 'evaluation': best_result[idx], 'formula': best_meta_formula})
         data.append({'defaults_type': 'vanilla', 'task_idx': idx, 'evaluation': baseline_result[idx]})
 
     df = pd.DataFrame(data=data)
@@ -47,13 +66,15 @@ def get_results_holdout(directory):
         baseline_holdout = results['baseline_holdout_score']
         best_avg_score = results['baseline_avg_performance']
         best_holdout = None
+        best_meta_formula = None
 
         for result in results['symbolic_defaults']:
             if result['avg_performance'] > best_avg_score:
                 best_avg_score = result['avg_performance']
                 best_holdout = result['holdout_score']
+                best_meta_formula = formula_str(result)
 
-        data.append({'defaults_type': 'symbolic', 'task_idx': task_id, 'evaluation': best_holdout})
+        data.append({'defaults_type': 'symbolic', 'task_idx': task_id, 'evaluation': best_holdout, 'formula': best_meta_formula})
         data.append({'defaults_type': 'vanilla', 'task_idx': task_id, 'evaluation': baseline_holdout})
 
     df = pd.DataFrame(data=data)
@@ -61,22 +82,30 @@ def get_results_holdout(directory):
 
 
 def run(args):
+    results_dir = os.path.join(args.results_dir, args.classifier_name)
     if args.per_task:
-        df = get_results_holdout(args.results_dir)
+        df_orig = get_results_holdout(results_dir)
+        title = 'Results on holdout tasks'
     else:
-        df = get_results_train(args.results_dir)
+        df_orig = get_results_train(results_dir)
+        title = 'Results on train set'
+    df_orig = df_orig.set_index('task_idx')
 
-    sns_plot = sns.boxplot(x='defaults_type', y='evaluation', data=df)
+    sns_plot = sns.boxplot(x='defaults_type', y='evaluation', data=df_orig).set_title(title)
     fig = sns_plot.get_figure()
-    fig.savefig(os.path.join(args.output_dir, 'results.pdf'))
+    fig.savefig(os.path.join(args.output_dir, '%s.pdf' % args.classifier_name))
 
-    df = df.pivot(index='task_idx', columns='defaults_type', values='evaluation')
-    df['difference'] = df.apply(lambda x: x['symbolic'] - x['vanilla'], axis=1)
+    df_pivot = df_orig.pivot(index=None, columns='defaults_type', values='evaluation')
+    df_pivot['difference'] = df_pivot.apply(lambda x: x['symbolic'] - x['vanilla'], axis=1)
+    # join with formula
+    df_pivot = df_pivot.join(df_orig[df_orig['defaults_type'] == 'symbolic']['formula'])
+    df_pivot.to_csv(os.path.join(args.output_dir, '%s.csv' % args.classifier_name))
+    print(df_pivot)
     wins = 0
     draws = 0
     loses = 0
 
-    for diff in df['difference']:
+    for diff in df_pivot['difference']:
         if diff > 0:
             wins += 1
         elif diff < 0:
