4c18a2629963eb987ce5cee54fa00533ac946667
janvanrijn <janvanrijn@gmail.com>
2019-01-15 14:49:28 -0500

    update multiple default scheduler

--- examples/vanilla/multiple_defaults_multi_exp.py
+++ examples/vanilla/multiple_defaults_multi_exp.py
@@ -13,15 +13,18 @@ def parse_args():
                         default=os.path.expanduser('~') + '/experiments/openml-defaults/vanilla_defaults/')
     parser.add_argument('--study_id', type=str, default='OpenML100', help='the tag to obtain the tasks from')
     parser.add_argument('--task_idx', type=int, default=None)
-    parser.add_argument('--metadata_file', type=str, default=metadata_file)
+    parser.add_argument('--metadata_files', type=str, nargs='+', default=metadata_file)
     parser.add_argument('--classifier_name', type=str, default='svc', help='scikit-learn flow name')
     parser.add_argument('--scoring', type=str, default='predictive_accuracy')
     parser.add_argument('--search_space_identifier', type=str, default='small')
     parser.add_argument('--minimize', action='store_true')
     parser.add_argument('--resized_grid_size', type=int, default=8)
     parser.add_argument('--n_defaults', type=int, default=32)
+    parser.add_argument('--n_configurations', type=int, default=2**13)
     parser.add_argument('--random_seed', type=int, default=42)
     parser.add_argument('--task_limit', type=int, default=None, help='For speed')
+    parser.add_argument('--n_estimators', type=int, default=64)
+    parser.add_argument('--minimum_evals', type=int, default=128)
     args_ = parser.parse_args()
     return args_
 
@@ -65,19 +68,20 @@ def run(args):
                     for task_id in all_task_ids:
                         openmldefaults.experiments.run_vanilla_surrogates_on_task(
                             task_id=task_id,
-                            classifier_name=args.classifier_name,
                             random_seed=args.random_seed,
                             search_space_identifier=args.search_space_identifier,
-                            metadata_file=args.metadata_file,
-                            resized_grid_size=args.resized_grid_size,
+                            metadata_files=args.metadata_file,
                             scoring=args.scoring,
                             minimize_measure=args.minimize,
                             n_defaults=args.n_defaults,
+                            n_configurations=args.n_configurations,
                             aggregate=aggregate,
                             a3r_r=a3r_r,
                             normalize_base=normalize_base,
                             normalize_a3r=normalize_a3r,
                             task_limit=args.task_limit,
+                            surrogate_n_estimators=args.n_estimators,
+                            surrogate_minimum_evals=args.minimum_evals,
                             output_directory=args.output_directory)
 
 
