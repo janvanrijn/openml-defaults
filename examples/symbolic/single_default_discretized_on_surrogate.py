6f48d4cafcf77c850d7adea9be7ecf7d07b12f3a
janvanrijn <janvanrijn@gmail.com>
2019-04-12 18:29:51 +0200

    updated single default discretized

--- examples/symbolic/single_default_discretized_on_surrogate.py
+++ examples/symbolic/single_default_discretized_on_surrogate.py
@@ -9,22 +9,24 @@ import os
 import pandas as pd
 import pickle
 import sklearn
-import sklearnbot
 import typing
 
 
 def parse_args():
-    metadata_file = '/home/janvanrijn/experiments/sklearn-bot/results/results__500__svc__predictive_accuracy.arff'
-    parser = argparse.ArgumentParser(description='Creates an ARFF file')
+    metadata_file = '/home/janvanrijn/projects/sklearn-bot/data/svc.arff'
+    parser = argparse.ArgumentParser()
     parser.add_argument('--output_directory', type=str, help='directory to store output',
                         default=os.path.expanduser('~') + '/experiments/openml-defaults/symbolic_defaults/')
     parser.add_argument('--study_id', type=str, default='OpenML100', help='the tag to obtain the tasks from')
     parser.add_argument('--task_idx', type=int, default=None)
     parser.add_argument('--metadata_file', type=str, default=metadata_file)
     parser.add_argument('--classifier_name', type=str, default='svc', help='scikit-learn flow name')
+    parser.add_argument('--search_space_identifier', type=str, default=None)
     parser.add_argument('--scoring', type=str, default='predictive_accuracy')
     parser.add_argument('--resized_grid_size', type=int, default=8)
     parser.add_argument('--random_seed', type=int, default=42)
+    parser.add_argument('--n_estimators', type=int, default=64)
+    parser.add_argument('--task_id_column', default='task_id', type=str)
     return parser.parse_args()
 
 
@@ -179,7 +181,7 @@ def run(args):
     if 34536 in study.tasks:
         study.tasks.remove(34536)
 
-    config_space = sklearnbot.config_spaces.get_config_space(args.classifier_name, args.random_seed)
+    config_space = openmldefaults.config_spaces.get_config_space(args.classifier_name, args.random_seed, args.search_space_identifier)
     configurations = openmldefaults.utils.generate_grid_configurations(config_space, 0, args.resized_grid_size)
 
     config_frame_orig = pd.DataFrame(configurations)
@@ -187,9 +189,12 @@ def run(args):
     quality_frame = openmlcontrib.meta.get_tasks_qualities_as_dataframe(study.tasks, False, -1, True)
 
     metadata_atts = openmldefaults.utils.get_dataset_metadata(args.metadata_file)
-    if args.scoring not in metadata_atts['measure']:
+    if args.scoring not in metadata_atts['col_measures']:
         raise ValueError('Could not find measure: %s' % args.scoring)
-    metadata_frame = openmldefaults.utils.metadata_file_to_frame(args.metadata_file, config_space, args.scoring)
+    metadata_frame = openmldefaults.utils.metadata_files_to_frame([args.metadata_file],
+                                                                  args.search_space_identifier,
+                                                                  [args.scoring],
+                                                                  task_id_column=args.task_id_column)
 
     surrogates = dict()
     for idx, task_id in enumerate(study.tasks):
@@ -207,7 +212,13 @@ def run(args):
             continue
 
         estimator, columns = openmldefaults.utils.train_surrogate_on_task(
-            task_id, config_space, setup_frame, args.scoring, 64, args.random_seed)
+            task_id,
+            config_space,
+            setup_frame,
+            args.scoring,
+            normalize=True,  # TODO: check whether neccessary
+            n_estimators=args.n_estimators,
+            random_seed=args.random_seed)
         if not np.array_equal(config_frame_orig.columns.values, columns):
             # if this goes wrong, it is due to the pd.get_dummies() fn
             raise ValueError('Column set not equal: %s vs %s' % (config_frame_orig.columns.values, columns))
